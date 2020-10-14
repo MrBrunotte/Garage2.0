350cb2288c3f747ead3efb9d3433e8d2f704d2d5
mrbrunotte <mrbrunotte@gmail.com>
2020-10-14 10:12:54 +0200

    Add enum VehicleType class

---
+++ Garage2.0/Models/VehicleType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage2._0.Models
+{
+    public enum VehicleType
+    {
+        // Enum for types of Vehicles
+        Motorcycle,
+        Bicycle,
+        Car,
+        SportsCar,
+        SUV,
+        CAB,
+        MiniVan,
+        Bus,
+        Truck
+    }
+}
