4ea56a74d14e13f6cbf77aba435ed0558f05ce79
mrbrunotte <mrbrunotte@gmail.com>
2020-10-14 10:32:58 +0200

    Add ParkedVehicle class

---
+++ Garage2.0/Models/ParkedVehicle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Garage2._0.Models
+{
+    public class ParkedVehicle
+    {
+        // Public so that it is accessible 
+        // Add Data Annotations for each property
+        public int ID { get; set; }
+
+        [Required, StringLength(30)]
+        public string VehicleType { get; set; }
+
+        [Required, StringLength(30)]
+        public string RegNum { get; set; }
+
+        [Required]
+        public string Color { get; set; }
+
+        [Required, StringLength(30)]
+        public string Make { get; set; }
+
+        [Required, StringLength(30)]
+        public string Model { get; set; }
+
+        [Required, Display(Name = "Number of Wheels")]
+        public int NumOfWheels { get; set; }
+
+        [DataType(DataType.Duration), Display(Name = "Duration of parking")]
+        public DateTime ArrivalTime { get; set; }
+    }
+}
