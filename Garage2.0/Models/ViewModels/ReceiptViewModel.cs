c182d184a62b9e3d814486f6e426aff106f62936
Torbjorn Granat <torbjorn.granat@gmail.com>
2020-10-15 11:58:45 +0200

    Formatting parking time and cost

--- Garage2.0/Models/ViewModels/ReceiptViewModel.cs
+++ Garage2.0/Models/ViewModels/ReceiptViewModel.cs
@@ -12,8 +12,13 @@ namespace Garage2._0.Models.ViewModels
         public DateTime ArrivalTime { get; set; }
         [Display(Name = "Time of Checkout")]
         public DateTime CheckOutTime { get; set; }
+
+        // https://docs.microsoft.com/en-us/dotnet/api/system.componentmodel.dataannotations.displayformatattribute.dataformatstring?view=netcore-3.1
+
         [Display(Name = "Parking Time")]
+        [DisplayFormat(DataFormatString = "{0:%d}d {0:%h}h {0:%m}m")]
         public TimeSpan Period { get; set; }
+        [DisplayFormat(DataFormatString = "{0:C}")]
         public double Cost { get; set; }
     }
 }
