4453838d78e128a978b8366c8238544c212df9c0
mrbrunotte <mrbrunotte@gmail.com>
2020-10-19 12:07:45 +0200

    Sorted Enum

--- Garage2.0/Models/VehicleType.cs
+++ Garage2.0/Models/VehicleType.cs
@@ -8,14 +8,14 @@ namespace Garage2._0.Models
     public enum VehicleType
     {
         // Enum for types of Vehicles
-        Motorcycle,
         Bicycle,
+        Bus,
+        CAB,
         Car,
+        Motorcycle,
+        MiniVan,
         SportsCar,
         SUV,
-        CAB,
-        MiniVan,
-        Bus,
         Truck
     }
 }
